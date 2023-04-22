486a56acbe4925a2cac5d02d037f149c126f12e3
samico <loouiz@gmail.com>
2023-04-21 22:12:41 -0300

    testes da q1 adaptados para o novo formato

--- src/main.py
+++ src/main.py
@@ -9,14 +9,6 @@ telefone = checa_campos_form.Telefone()
 datetime = checa_campos_form.Datetime()
 numero = checa_campos_form.Numero()
 
-# verificador_de_nome = checa_campos_form.Verificador(nome)
-# verificador_de_email = checa_campos_form.Verificador(email)
-# verificador_de_senha = checa_campos_form.Verificador(senha)
-# verificador_de_cpf = checa_campos_form.Verificador(cpf)
-# verificador_de_telefone = checa_campos_form.Verificador(telefone)
-# verificador_de_datetime = checa_campos_form.Verificador(datetime)
-# verificador_de_numero = checa_campos_form.Verificador(numero)
-
 opcoes = {
     "1": nome,
     "2": email,
@@ -31,9 +23,9 @@ print("Escolha uma opção: ")
 for numero, opcao in opcoes.items():
     print(f"{numero} - {str(opcao)}")
 escolha = opcoes[input("Número escolhido: ")]
+
 verificador = checa_campos_form.Verificador(escolha)
-
 entrada = input("Qual o valor a ser verificado? ")
-
 resultado = verificador.reconhecer(entrada)
+
 print(f"{str(escolha)} é {'valido(a)' if resultado else 'invalido(a)'}")
