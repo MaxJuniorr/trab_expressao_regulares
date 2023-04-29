93555bcbadbd462afdb2bbc9b2503ce811efb73b
MaxJuniorr <maxjr.dev@gmail.com>
2023-04-29 09:07:22 -0300

    remocao do relatorio.doc

--- src/checa_arranjos_familiares/checa_arranjos_familiares.py
+++ src/checa_arranjos_familiares/checa_arranjos_familiares.py
@@ -101,7 +101,7 @@ def validar_arranjo_foxtrot(arranjo: str) -> bool:
     return bool(reconhecido)
 
 
-def validar_arranjo_golf(arranjo: str) -> bool:
+def validar_arranjo_golf(arranjo: str, x: int, y: int) -> bool:
     """Arranjo de no mínimo x ∈ N e no máximo y ∈ N, com x > 0, y > 0 , e x ≤ y , de
     adultos (Hs ou Ms) mais velhos que os filhos, com qualquer quantidade de filhos
     homens e mulheres, mas que os três filhos mais novos não foram homens.
@@ -112,7 +112,7 @@ def validar_arranjo_golf(arranjo: str) -> bool:
         {boll}
     """
 
-    regra = r"^(H|M)+(((h|m)*(m|hm|mh|mhh))|hh)$"
+    regra = r"^$"
     reconhecido = re.search(regra, arranjo)
     return bool(reconhecido)
 
