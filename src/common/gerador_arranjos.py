353b2c2f59983f9e990330a68d5619b8f00ba630
MaxJuniorr <maxjr.dev@gmail.com>
2023-04-29 17:09:38 -0300

    implementação dos geradores arranjos. (o último está incompleto)

--- src/common/gerador_arranjos.py
+++ src/common/gerador_arranjos.py
@@ -13,105 +13,215 @@ def criar_casais_homo() -> str:
 def criar_nsais_platonico(n:int) -> str:
     return ''.join(random.choices('HM', k=n))
 
+########## filhos
+# [WARNING] Regras que definem tamanho devem ser aplicadas primeiro
 
-def floor6_prole(n: int) -> str:
+def iniciar_prole() -> str:
+    '''Cria uma prole combinando "h" e "m" de tamanho n'''
+    n = random.randint(100)
+    return ''.join(random.choices('hm', k=n))
+
+
+def floor6_prole(cadeia: str) -> str:
     """Retorna uma prole de tamanho mínimo 6."""
-    pass
+    tam = len(cadeia)
+    if tam >= 6:
+        return cadeia
+    return cadeia + ''.join(random.choices('hm', k=6-tam))
 
 
-def floor_h(n: int) -> str:
+def floor_h(cadeia: str, n: int) -> str:
     """Retorna uma string com pelo menos n 'h's."""
-    pass
+
+    # resolver problema com len(cadeia) < n ocorre loop infinito
+    if len(cadeia) <= n:
+        return ''.join(random.choices('h', k=n))
+    
+    qtd = cadeia.count('h')
+    controle = n - qtd
+    i = 0
+    while i < controle:
+        indice = random.randint(0, len(cadeia) - 1)
+        if cadeia[indice] != "h":
+            cadeia = cadeia[:indice] + "h" + cadeia[indice + 1:]
+            i += 1
+    return cadeia
 
 
-def floor_m(n: int) -> str:
+def floor_m(cadeia: str, n: int) -> str:
     """Retorna uma string com pelo menos n 'm's."""
-    pass
+    # resolver problema com len(cadeia) < n ocorre loop infinito
+    if len(cadeia) <= n:
+        return ''.join(random.choices('m', k=n))
+    
+    qtd = cadeia.count('m')
+    controle = n - qtd
+    i = 0
+    while i < controle:
+        indice = random.randint(0, len(cadeia) - 1)
+        if cadeia[indice] != "m":
+            cadeia = cadeia[:indice] + "m" + cadeia[indice + 1:]
+            i += 1
+    return cadeia
 
 
-def ceil_h(n:int) -> str:
+def ceil_h(cadeia: str, n: int) -> str:
     """Retorna uma string com no máximo n 'h's."""
-    pass
+
+    qtd = cadeia.count('h')
+    if qtd <= n:
+        return cadeia
+
+    controle = qtd - n
+    i = 0
+    while i < controle:
+        indice = random.randint(0, len(cadeia) - 1)
+        if cadeia[indice] != "m":
+            cadeia = cadeia[:indice] + "m" + cadeia[indice + 1:]
+            i += 1
+    return cadeia
 
 
-def ceil_m(n: int) -> str:
+def ceil_m(cadeia: str, n: int) -> str:
     """Retorna uma string com no máximo n 'm's."""
-    pass
+    
+    qtd = cadeia.count('m')
+    if qtd <= n:
+        return cadeia
 
-def impar_h() -> str:
+    controle = qtd - n
+    i = 0
+    while i < controle:
+        indice = random.randint(0, len(cadeia) - 1)
+        if cadeia[indice] != "h":
+            cadeia = cadeia[:indice] + "h" + cadeia[indice + 1:]
+            i += 1
+    return cadeia
+
+def impar_h(cadeia: str) -> str:
     """Retorna uma string com uma quantidade impar de 'h's."""
-    pass
+    num_h = cadeia.count('h')
+    if num_h % 2 != 0:
+        return cadeia
+    indice = random.randint(len(cadeia))
+    cadeia = cadeia[:indice] + 'h' + cadeia[indice+1:]
+    return cadeia
+    
 
-
-def impar_m() -> str:
+def impar_m(cadeia: str) -> str:
     """Retorna uma string com uma quantidade impar de 'm's."""
-    pass
+    num_m = cadeia.count('m')
+    if num_m % 2 != 0:
+        return cadeia
+    indice = random.randint(len(cadeia))
+    cadeia = cadeia[:indice] + 'm' + cadeia[indice+1:]
+    return cadeia
 
 
-def par_h() -> str:
+def par_h(cadeia: str) -> str:
     """Retorna uma string com uma quantidade par de 'h's."""
-    pass
+    num_h = cadeia.count('h')
+    if num_h % 2 == 0:
+        return cadeia
+    indice = random.randint(len(cadeia))
+    cadeia = cadeia[:indice] + 'h' + cadeia[indice+1:]
+    return cadeia
 
 
-def par_m() -> str:
+def par_m(cadeia: str) -> str:
     """Retorna uma string com uma quantidade par de 'm's."""
-    pass
+    num_m = cadeia.count('h')
+    if num_m % 2 == 0:
+        return cadeia
+    indice = random.randint(len(cadeia))
+    cadeia = cadeia[:indice] + 'm' + cadeia[indice+1:]
+    return cadeia
 
 
-def mais_velho_h() -> str:
+def mais_velho_h(cadeia: str) -> str:
     """Retorna uma string em que o filho mais velho é h"""
-    pass
+    if cadeia[-1] == 'h':
+        return cadeia
+    cadeia = cadeia[:-1] + 'h'
+    return cadeia
 
 
-def mais_velho_m() -> str:
+def mais_velho_m(cadeia: str) -> str:
     """Retorna uma string em que o filho mais velho é m"""
-    pass
+    if cadeia[-1] == 'm':
+        return cadeia
+    cadeia = cadeia[:-1] + 'm'
+    return cadeia
 
 
-def mais_novo_h() -> str:
+def mais_novo_h(cadeia: str) -> str:
     """Retorna uma string em que o filho mais novo é h"""
-    pass
+    if cadeia[0] == 'h':
+        return cadeia
+    cadeia = 'h' + cadeia[1:]
+    return cadeia
 
 
-def mais_novo_m() -> str:
+def mais_novo_m(cadeia: str) -> str:
     """Retorna uma string em que o filho mais novo é m"""
-    pass
+    if cadeia[0] == 'm':
+        return cadeia
+    cadeia = 'm' + cadeia[1:]
+    return cadeia
 
 
-def casal_primeiro() -> str:
+def casal_primeiro(cadeia: str) -> str:
     """Retorna uma string em que os dois primeiros elementos formam um
     casal.
     """
-    pass
+    if (cadeia[0:2] == 'hm') or (cadeia[0:2] == 'mh'):
+        return cadeia
+    casal_random = random.choice(('hm', 'mh'))
+    cadeia = casal_random + cadeia[2:]
+    return cadeia
 
 
-def casal_ultimo() -> str:
+def casal_ultimo(cadeia: str) -> str:
     """Retorna uma string em que os dois últimos elementos formam um
     casal.
     """
-    pass
+    if (cadeia[0:2] == 'hm') or (cadeia[0:2] == 'mh'):
+        return cadeia
+    casal_random = random.choice(('hm', 'mh'))
+    cadeia = cadeia[:-2] + casal_random
+    return cadeia
 
 
-def filhos_alternados() -> str:
+# [WARNING] Prioridade
+def filhos_alternados(cadeia: str) -> str:
     """Retorna uma string em que os filhos são alternados entre h e m."""
-    pass
+    nova_cadeia = random.choice(('hm', 'mh'))
+    nova_cadeia *= len(cadeia)
+    return nova_cadeia
 
 
-def nao_filhos_h_consecutivos() -> str:
+# [NOTE] Testar isso
+def nao_filhos_h_consecutivos(cadeia: str) -> str:
     """Retorna uma string em que não há filhos h consecutivos."""
-    pass
+    if 'hh' not in cadeia:
+        return cadeia
+    indice = cadeia.index('hh')
+    casal_random = random.choice(('hm', 'mh'))
+    cadeia = cadeia[:indice] + casal_random + cadeia[indice+2:]
 
 
 def nao_filhos_m_consecutivos() -> str:
     """Retorna uma string em que não há filhos m consecutivos."""
-    pass
+    if 'mm' not in cadeia:
+        return cadeia
+    indice = cadeia.index('mm')
+    casal_random = random.choice(('hm', 'mh'))
+    cadeia = cadeia[:indice] + casal_random + cadeia[indice+2:]
 
 
 def nao_ultimos3_h_consecutivos() -> str:
     """Retorna uma string em que não há 3 'h's consecutivos no final."""
-    pass
-
-
-def criar_prole(n: int) -> str:
-    '''Cria uma prole combinando "h" e "m" de tamanho n'''
-    return ''.join(random.choices('hm', k=n))
+    if 'hhh' != cadeia[-3:]:
+        return cadeia
+    casal_random = random.randin(('hm', 'mh'))
+    cadeia = cadeia[:indice] + casal_random + cadeia[indice+2:]
